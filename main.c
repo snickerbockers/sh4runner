f82881f30d9a56bc42b5a19b342b303076d8fff6
snickerbockers <snickerbockers@washemu.org>
2019-12-13 03:40:43 -0800

    print wherther or not the controller is detected

--- main.c
+++ main.c
@@ -64,6 +64,8 @@ void *get_romfont_pointer(void);
 
 static void volatile *cur_framebuffer;
 
+static int check_controller(void);
+
 static void configure_video(void) {
     // Hardcoded for 640x476i NTSC video
     SPG_HBLANK_INT = 0x03450000;
@@ -307,6 +309,76 @@ static void wait_vblank(void) {
     REG_ISTNRM = (1 << 3);
 }
 
+#define REG_MDSTAR (*(unsigned volatile*)0xa05f6c04)
+#define REG_MDTSEL (*(unsigned volatile*)0xa05f6c10)
+#define REG_MDEN   (*(unsigned volatile*)0xa05f6c14)
+#define REG_MDST   (*(unsigned volatile*)0xa05f6c18)
+#define REG_MSYS   (*(unsigned volatile*)0xa05f6c80)
+#define REG_MDAPRO (*(unsigned volatile*)0xa05f6c8c)
+#define REG_MMSEL  (*(unsigned volatile*)0xa05f6ce8)
+
+static void volatile *align32(void volatile *inp) {
+    char volatile *as_ch = (char volatile*)inp;
+    while (((unsigned)as_ch) & 31)
+        as_ch++;
+    return (void volatile*)as_ch;
+}
+#define MAKE_PHYS(addr) ((void*)((((unsigned)addr) & 0x1fffffff) | 0xa0000000))
+
+static void wait_maple(void) {
+    while (!(REG_ISTNRM & (1 << 12)))
+           ;
+
+    // clear the interrupt
+    REG_ISTNRM |= (1 << 12);
+}
+
+static int check_controller(void) {
+    // clear any pending interrupts (there shouldn't be any but do it anyways)
+    REG_ISTNRM |= (1 << 12);
+
+    // disable maple DMA
+    REG_MDEN = 0;
+
+    // make sure nothing else is going on
+    while (REG_MDST)
+        ;
+
+    // 2mpbs transfer, timeout after 1ms
+    REG_MSYS = 0xc3500000;
+
+    // trigger via CPU (as opposed to vblank)
+    REG_MDTSEL = 0;
+
+    // let it write wherever it wants, i'm not too worried about rogue DMA xfers
+    REG_MDAPRO = 0x6155407f;
+
+    // construct packet
+    static unsigned volatile devinfo0[1024];
+    static unsigned volatile frame[36 + 31];
+
+    unsigned volatile *framep = (unsigned*)MAKE_PHYS(align32(frame));
+    unsigned volatile *devinfo0p = (unsigned*)MAKE_PHYS(align32(devinfo0));
+
+    framep[0] = 0x80000000;
+    framep[1] = ((unsigned)devinfo0p) & 0x1fffffff;
+    framep[2] = 0x2001;
+
+    // set SB_MDSTAR to the address of the packet
+    REG_MDSTAR = ((unsigned)framep) & 0x1fffffff;
+
+    // enable maple DMA
+    REG_MDEN = 1;
+
+    // begin the transfer
+    REG_MDST = 1;
+
+    wait_maple();
+
+    // transfer is now complete, receive data
+    return (devinfo0p[0] & 0xff) != 0xff;
+}
+
 /*
  * our entry point (after _start).
  *
@@ -328,7 +400,11 @@ int dcmain(int argc, char **argv) {
 
     configure_video();
 
+    wait_vblank();
+
     for (;;) {
+        int have_controller = check_controller();
+
         clear_screen(get_backbuffer(), make_color(0, 0, 0));
 
         drawstring(get_backbuffer(), normal_font, "WELCOME TO SH4RUNNER", 1, 0);
@@ -342,6 +418,11 @@ int dcmain(int argc, char **argv) {
             drawstring(get_backbuffer(), normal_font, arm_msg, 3, 0);
         else
             drawstring(get_backbuffer(), fail_font, "NO MESSAGE RECEIVED FROM ARM", 3, 0);
+
+        if (have_controller)
+            drawstring(get_backbuffer(), success_font, "controller detected", 4, 0);
+        else
+            drawstring(get_backbuffer(), fail_font, "no controller detected", 4, 0);
 
         wait_vblank();
         swap_buffers();
